80fcda11b35d1fcb972909333c4edb8d3ab808d8
Julio <juliosilvacoutinho@gmail.com>
2022-07-26 16:58:29 -0300

    start of integration with api

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,7 +3,7 @@ const multer = require("multer")
 const uploadConfig = require("../configs/upload")
 
 const UsersController = require("../controllers/UsersController")
-const UsersAvatarController = require("../controllers/UsersAvatarController")
+const UserAvatarController = require("../controllers/UserAvatarController")
 
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
@@ -12,12 +12,12 @@ const upload = multer(uploadConfig.MULTER)
 
 
 const usersController = new UsersController()
-const usersAvatarController = new UsersAvatarController()
+const userAvatarController = new UserAvatarController()
 
 
 usersRoutes.post("/", usersController.create)
 usersRoutes.put("/", ensureAuthenticated, usersController.update)
-usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), usersAvatarController.update)
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 
 module.exports =  usersRoutes;
