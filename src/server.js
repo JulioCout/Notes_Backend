18570c3bb5cf4a122e3f9ba1a8e3ffcc34b5b154
Julio Coutinho <juliosilvacoutinho@gmail.com>
2022-08-03 20:05:04 -0300

    variable created

--- src/server.js
+++ src/server.js
@@ -38,5 +38,5 @@ app.use(( error, req, res, next ) => {
 });
 
 //Server port and feedback
-const port = 3333
+const port = process.env.PORT || 3333
 app.listen(port, () => console.log(`Server is running on port ${port}`))
