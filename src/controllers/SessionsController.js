491524a449c93cd2d34c3bcd13c9f2f890fb631b
Julio Coutinho <juliosilvacoutinho@gmail.com>
2022-08-03 17:29:36 -0300

    sessionsController correction

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -2,7 +2,7 @@ const knex = require("../database/knex")
 const AppError = require("../utils/AppError")
 const { compare } = require("bcryptjs")
 const authConfig = require("../configs/auth")
-const { sign } = require("jsonWebToken")
+const { sign } = require("jsonwebtoken")
 
 
 class SessionsController {
